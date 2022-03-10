cf3f0aa6f1da43b670ab8144d3f46eee3733c189
21BCA71 <96732854+21BCA71@users.noreply.github.com>
2022-03-10 21:48:46 +0530

    Add files via upload

---
+++ Solution/prg9.py
@@ -0,0 +1,9 @@
+#was to input any number an print its sum
+
+a=int(input("Enter any number:"))
+sum=0
+b=a
+while(a!=0):
+    sum=sum+a%10
+    a=a//10
+print("sum of it digit of {} is {}".format(b,sum))
