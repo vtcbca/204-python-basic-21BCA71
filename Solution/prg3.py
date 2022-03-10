745c0d674e357072485406dd2e76f6566555cdae
21BCA71 <96732854+21BCA71@users.noreply.github.com>
2022-03-10 19:36:52 +0530

    Add files via upload

---
+++ Solution/prg3.py
@@ -0,0 +1,7 @@
+#was to enter any number and check weather it is even or odd
+
+n=int(input("Enter Any Number:"))
+if(n%2==0):
+    print("{} is an even number".format(n))
+else:
+    print("{} is a odd number".format(n))
