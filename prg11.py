be1c6679933a27ff2513c3fd29ae95a9b6705f85
21BCA71 <96732854+21BCA71@users.noreply.github.com>
2022-03-17 11:51:12 +0530

    Add files via upload

---
+++ prg11.py
@@ -0,0 +1,13 @@
+#WAS to enter any number and check number is armstrong number or not.
+
+a=int(input("Enter any Number:"))
+c=0
+d=a
+while(a>0):
+    b=a%10
+    c=c+b*b*b
+    a=a//10
+if(d==c):
+    print("{} it is a armstrong number".format(c))
+else:
+    print("{} it is not anrmstrong number".format(c))
